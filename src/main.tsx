731fedb961d6eb0d0c757a1e3d9d7239d3599297
Fábio Santana <29023946+thesekcy@users.noreply.github.com>
2023-08-04 22:58:44 -0300

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './routes.tsx'
+import { inject } from '@vercel/analytics'
 
 import '@fontsource-variable/epilogue/wght.css'
 import '@fontsource-variable/inter/slnt.css'
 
 import './styles/global.scss'
 import { AuthProvider } from './contexts/auth/authProvider.tsx'
+
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
